cdd33b3dc610b2c05c45f74c09708f4353096226
RBeltsov <roman.beltsov@texturacorp.com>
2016-09-05 21:08:38 +0300

    commit work

--- mysite/views.py
+++ mysite/views.py
@@ -1,12 +1,16 @@
 from random import shuffle
 
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template import RequestContext
 from django.template.context_processors import csrf
+from django.views.decorators.csrf import csrf_protect
 
 from mysite.forms import AddNewEvent
 from mysite.models import UploadForm, Upload, Events, Locations
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+
+
 # Create your views here.
 
 
@@ -31,20 +35,19 @@ def events_list(request, location_id=None):
         return render(request, 'mysite/events_list.html', {'location_events': all_events,
                                                            'locations': locations,
                                                            'priority_events': priority_events,
-                                                            'current_location': current_location})
-
-
+                                                           'current_location': current_location})
 
 
 def events_details(request, location_id, pk):
     event_data = Events.objects.get(id=pk)
     current_location = Locations.objects.get(id=location_id)
+    locations = Locations.objects.all()
     return render(request, 'mysite/event_details.html', {'event_data': event_data,
-                                                         'current_location': current_location})
+                                                         'current_location': current_location,
+                                                         'locations': locations})
 
 
 def add_event_form(request):
-    #posts = AddNewEvent
     context = {}
     context['EventsMod'] = AddNewEvent
     context.update(csrf(request))
@@ -56,6 +59,6 @@ def add_event_form(request):
         new_event.image = request.FILES['image']
         new_event.start_time = request.POST['start_time']
         new_event.start_date = request.POST['start_date']
-        new_event.save()
-        return redirect('event_details', pk=new_event.pk)
+        res = new_event.save()
+        return redirect('event_details', location_id=res.location.id, pk=res.pk)
     return render(request, 'mysite/add_event_form.html', context)
