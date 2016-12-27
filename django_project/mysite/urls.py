44d03f50e16023de3c2316b892b124e57f487a0e
Roman <dell.oxl@gmail.com>
2016-12-27 18:53:52 +0300

    + robots.txt

--- django_project/mysite/urls.py
+++ django_project/mysite/urls.py
@@ -4,14 +4,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [url(r'^admin/service/$', views.my_admin_view),
-                        url(r'^$', views.events_list, name='events_list'),
-                        url(r'^adm/$', views.admin_list, name='adm'),
-                        url(r'^adm/jserv/$', views.jservice, name='jservice'),
-                        url(r'^jdata/$', views.jdata, name='jdata'),
-                        url(r'^tag/$', views.set_tags, name='tag_it'),
-                        url(r'^set_user_location/$', views.set_user_location, name='set_user_location'),
-                        url(r'^(?P<site_screen_name>[a-z]+)/$', views.events_list, name='location_events'),
-                        url(r'^add_event_form/$', views.add_event_form, name='add_event_form'),
-                        url(r'^added_successfully/$', views.added_successfully, name='added_successfully'),
-                        url(r'^(?P<site_screen_name>[a-z]+)/(?P<pk>[0-9]+)/(?P<title_translit>[\w\-.%]*)$', views.events_details, name='event_details'),
-                        ]
+               url(r'^$', views.events_list, name='events_list'),
+               url(r'^adm/$', views.admin_list, name='adm'),
+               url(r'^adm/jserv/$', views.jservice, name='jservice'),
+               url(r'^jdata/$', views.jdata, name='jdata'),
+               url(r'^tag/$', views.set_tags, name='tag_it'),
+               url(r'^set_user_location/$', views.set_user_location, name='set_user_location'),
+               url(r'^(?P<site_screen_name>[a-z]+)/$', views.events_list, name='location_events'),
+               url(r'^add_event_form/$', views.add_event_form, name='add_event_form'),
+               url(r'^added_successfully/$', views.added_successfully, name='added_successfully'),
+               url(r'^(?P<site_screen_name>[a-z]+)/(?P<pk>[0-9]+)/(?P<title_translit>[\w\-.%]*)$', views.events_details,
+                   name='event_details'),
+               ]
